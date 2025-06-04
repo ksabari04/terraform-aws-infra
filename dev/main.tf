6371309d37aef9cf38d0afff2ce5ab520fb888f2
ksabari04 <vasansabari608@gmail.com>
2025-06-04 19:40:47 +0530

    Initial commit

---
+++ dev/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags = {
+    Name = "example-${terraform.workspace}"
+  }
+}
---
+++ dev/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  default = "ami-0c55b159cbfafe1f0"  # Example for ap-south-1
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
