bd85f2b52c37f78f8373230e26d65ad669ce0586
ksabari04 <147916315+ksabari04@users.noreply.github.com>
2025-06-07 12:23:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-sabari"
-    key            = "placeholder.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
